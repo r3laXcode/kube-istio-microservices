0675d64183946da00008c8a4fece6d4ad341c1a4
rxcode <rlopezaldana@gmail.com>
2019-06-12 21:10:18 -0500

    add service frontend

--- frontend/pages/servicio-a/index.vue
+++ frontend/pages/servicio-a/index.vue
@@ -2,29 +2,52 @@
   <section class="container">
     <div class="text-center">
       <h3 class="title">
-        Servicio A
+        Servicio A - Interno
       </h3>
+      <span >{{url}}</span>
     </div>
     <div>
        <p v-if="message"> {{message}} </p>
+       <section v-if="data">
+         <div class="row" style="padding-top:2em;">
+            <template v-for="item in data">
+              <div class="col-md-4 box" :key="item.id">
+                  <h3>{{item.name}}</h3>
+                  <span>{{item.email}}</span>
+              </div>
+            </template>
+         </div>
+       </section>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name:'servicio-a',
+  name:'servicio-ext',
   mounted() {
     this.loadData();
   },
   data() {
     return {
-      message:''
+      message:'',
+      url:'http://servicioa:5001',
+      data:[]
     }
   },
   methods:{
-    loadData(){
-      this.message = 'cargando ...'
+    async loadData(){
+      this.message = 'cargando ...';
+      try {
+          const response = await this.$axios.$get(this.url);
+          console.log(response);
+          this.message = '';
+          this.data = response;
+
+      } catch(err) {
+          console.log(err);
+      }
+
     }
   }
 }
@@ -40,6 +63,11 @@ export default {
   color: #35495e;
   letter-spacing: 1px;
   padding:1em;
+}
+
+
+.box {
+  min-height: 100px;
 }
 
 
